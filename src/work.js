a0ff11dd3c05d4e7acd35188afc49ea51a448216
tukki <tukki0210@gmail.com>
2020-09-08 14:11:29 +0000

    立体が回転した

---
+++ src/work.js
@@ -0,0 +1,33 @@
+/**@jsx jsx */
+import { useRef } from 'react'
+import { Canvas, useFrame } from 'react-three-fiber'
+import { css, jsx } from '@emotion/core'
+
+const theme = css`
+   *{ width: 100vw;
+      height: 100vh;
+   }
+`;
+
+const Thing = () => {
+    const ref = useRef();
+
+    useFrame(() => {
+        ref.current.rotation.y += 0.01
+    });
+
+    return (
+        <mesh ref={ref}>
+            <boxGeometry attach='geometry' args={[400, 400, 400]} />
+            <meshNormalMaterial attach='material' />
+        </mesh>
+    );
+};
+
+export const Work = () => (
+    <div css={theme}>
+        <Canvas camera={{ position: [0, 0, 1000] }}>
+            <Thing />
+        </Canvas>
+    </div>
+)
